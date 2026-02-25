a85d92998d191a10dd09d57dfeac6e6b46b6d5a3
srikk <122169588+py-code314@users.noreply.github.com>
2026-02-25 09:58:29 -0500

    feat: Add a new publisher to db

--- controllers/publishersController.js
+++ controllers/publishersController.js
@@ -22,9 +22,7 @@ const validatePublisher = [
   body('name').trim().notEmpty().withMessage(`Name ${emptyErr}`),
   body('email')
     .trim()
-    .notEmpty()
-    .withMessage(`Name ${emptyErr}`)
-    .bail()
+    .optional({values: 'falsy'})
     .isEmail()
     .withMessage(`Email ${emailErr}`),
 ]
@@ -65,13 +63,35 @@ const publisher_create_post = [
 
     // Show errors if validation fails
     if (!errors.isEmpty()) {
-      res.send('Error message')
-      return
+      return res.status(400).render('pages/publishers/publisher-form', {
+        title: 'Add Publisher',
+        publisher: req.body,
+        errors: errors.array(),
+      })
     }
 
-    const { name, email } = matchedData(req)
-    await addPublisher(name, email)
-    res.send('Publisher added successfully')
+    try {
+      const { name, email } = matchedData(req)
+      await addPublisher(name, email)
+      res.redirect('/publishers')
+    } catch (err) {
+      console.error(err)
+
+      // Default
+      let statusCode = 500
+      let errorMsg = 'A database error occurred. Please try again later.'
+      // Update for UNIQUE constraint violation
+      if (err.code === '23505') {
+        statusCode = 409
+        errorMsg = 'Publisher name must be unique.'
+      }
+
+      return res.status(statusCode).render('pages/publishers/publisher-form', {
+        title: 'Add Publisher',
+        publisher: req.body,
+        errors: [{ msg: errorMsg }],
+      })     
+    }    
   },
 ]
 
